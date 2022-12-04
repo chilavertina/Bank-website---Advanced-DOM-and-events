e9bbafbbc5894df8ce4ed59d703df7957f42e5c0
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-04 20:02:33 +0100

    smooth scrolling za linkove u heading-u

--- script.js
+++ script.js
@@ -66,7 +66,18 @@ btnScrollTo.addEventListener('click', function (e) {
 
 //////////////////////////////////////////////
 
-//PAGE NAVIGATION
+//Page navigation
+
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
 
 ////////////////////////////////////////////////
 ///////////////////////////////////////////////
