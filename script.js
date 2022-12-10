1e3c807123d05ebfdf86e98768399d05176e8c0f
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-10 11:48:08 +0100

    aktiviranje dots prilikom listanja slajdova

--- script.js
+++ script.js
@@ -252,6 +252,16 @@ const createDots = function () {
 };
 createDots();
 
+const activateDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+
 const goToSlide = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
@@ -267,6 +277,7 @@ const nextSlide = function () {
     curSlide++;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
 // Previous slide
@@ -277,6 +288,7 @@ const prevSlide = function () {
     curSlide--;
   }
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
 btnRight.addEventListener('click', nextSlide);
@@ -291,6 +303,7 @@ dotContainer.addEventListener('click', function (e) {
   if (e.target.classList.contains('dots__dot')) {
     const slide = e.target.dataset.slide; // destruktuiranje koda const {slide} = e.target.dataset;
     goToSlide(slide);
+    activateDot(slide);
   }
 });
 ////////////////////////////////////////////////
