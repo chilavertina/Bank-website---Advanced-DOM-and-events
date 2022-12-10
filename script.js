f016541ce26ed6f1a1d22987b4de1add8368ab99
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-12-10 11:37:51 +0100

    aktiviranje dots uz pomoc eventListener-a

--- script.js
+++ script.js
@@ -286,6 +286,13 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'ArrowLeft') prevSlide();
   e.key === 'ArrowRight' && nextSlide();
 });
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const slide = e.target.dataset.slide; // destruktuiranje koda const {slide} = e.target.dataset;
+    goToSlide(slide);
+  }
+});
 ////////////////////////////////////////////////
 ///////////////////////////////////////////////
 /*
